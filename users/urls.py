eaf719a346c9d7daff4421cb9c8f937331983114
Ghifari Aulia Azhar Riza <ghifari.auliaz@gmail.com>
2022-05-09 18:27:47 +0700

    edit login views

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('user/', views.get_user),
-    path('login/', views.login),
+    path('login/', views.LoginAPI.as_view(), name='login'),
     path('register/', views.register),
     path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
     path('logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall')
--- users/views.py
+++ users/views.py
@@ -3,7 +3,9 @@ from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.auth import AuthToken, TokenAuthentication
 from .serializers import RegisterSerializer
-from rest_framework import status
+from rest_framework import status, permissions, request
+from knox.views import LoginView
+from django.contrib.auth import login
 
 def serialize_user(user):
     return {
@@ -12,17 +14,15 @@ def serialize_user(user):
         "name": user.first_name,
     }
 
-@api_view(['POST'])
-def login(request):
-    serializer = AuthTokenSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    user = serializer.validated_data['user']
-    _, token = AuthToken.objects.create(user)
-    return Response({
-        'user_info': serialize_user(user),
-        'token': token
-    })
-        
+class LoginAPI(LoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)      
 
 @api_view(['POST'])
 def register(request):
@@ -34,7 +34,6 @@ def register(request):
             "user_info": serialize_user(user),
             "token": token
         })
-
 
 @api_view(['GET'])
 def get_user(request):
